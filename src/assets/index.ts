a070ec3ab90d9ab3f0726af85652c33164cee9a6
shahinalom <shahinalom3511371@gmail.com>
2024-12-15 23:59:15 +0500

    banner update

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,9 +1,17 @@
 
-
 import logo from './logo.svg'
 import banner from './banner.png'
+import bannerone from "./33.webp"
+import bannertwo from "./34.webp"
+import bannerthree from "./35.webp"
+import bannerfour from "./36.webp"
+
 
 export {
       logo,
-      banner
+      banner,
+      bannerone,
+      bannertwo,
+      bannerthree,
+      bannerfour
 }
